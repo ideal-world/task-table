cd9bf0665cfad4a0fe3a04451c58034efc6111fe
gudaoxuri <i@sunisle.org>
2023-11-14 13:30:25 +0800

    Add Alert component with different kinds of alerts

---
+++ src/components/common/Alert.ts
@@ -0,0 +1,69 @@
+export enum AlertKind {
+  ERROR,
+  WARNING,
+  INFO,
+  SUCCESS,
+}
+
+export function showAlert(message: string, showTimeSec: number, kind: AlertKind = AlertKind.WARNING, attachEle: HTMLElement = document.body) {
+  let alertDiv = ``
+  let messageDiv = `<span>` + message + `</span>`
+  switch (kind) {
+    case AlertKind.ERROR: {
+      alertDiv = `  <div class="alert alert-error fixed z-[10000] top-0 left-0 p-1 rounded-lg">
+    <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+        d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+    </svg>
+   `+ messageDiv + `
+  </div>`
+      break
+    }
+    case AlertKind.WARNING: {
+      alertDiv = `  <div class="alert alert-warning fixed z-[10000] top-0 left-0 p-1 rounded-lg">
+    <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+        d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+    </svg>
+   `+ messageDiv + `
+  </div>`
+      break
+    }
+    case AlertKind.INFO: {
+      alertDiv = `  <div class="alert alert-info fixed z-[10000] top-0 left-0 p-1 rounded-lg">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current shrink-0 w-6 h-6">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+        d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+    </svg>
+   `+ messageDiv + `
+  </div>`
+      break
+    }
+    case AlertKind.SUCCESS: {
+      alertDiv = `  <div class="alert alert-success fixed z-[10000] top-0 left-0 p-1 rounded-lg">
+    <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+        d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+    </svg>
+   `+ messageDiv + `
+  </div>`
+      break
+    }
+    default: {
+      alertDiv = `  <div class="alert fixed z-[10000] top-0 left-0 p-1 rounded-lg">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-info shrink-0 w-6 h-6">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+        d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+    </svg>
+   `+ messageDiv + `
+  </div>`
+      break
+    }
+  }
+  let alertEle = document.createElement('div')
+  alertEle.innerHTML = alertDiv
+  attachEle.append(alertEle)
+  setTimeout(() => {
+    alertEle.remove()
+  }, showTimeSec * 1000);
+}
