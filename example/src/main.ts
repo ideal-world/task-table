8ffaade24deb0da11cf198c0d4184fa1694b7872
gudaoxuri <i@sunisle.org>
2024-06-12 12:04:17 +0800

    Remove example.

--- example/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import IwTaskTable from 'task-table'
-import App from './App.vue'
-import 'task-table/dist/style.css'
-
-createApp(App)
-  .use(IwTaskTable)
-  .mount('#app')
